652f2d0ce35e514e2842aa3b88d0bca67aaa0df7
Hardik Arora <118887883+HardikArora0843@users.noreply.github.com>
2026-07-27 02:13:38 +0530

    Update redis.js

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -6,8 +6,8 @@ const redisClient = createClient({
     socket: {
         // host: 'redis-16057.c98.us-east-1-4.ec2.redns.redis-cloud.com',
         // port: 16057
-        host: 'redis-18092.crce182.ap-south-1-1.ec2.cloud.redislabs.com',
-        port: 18092
+        host: 'redis-10992.crce182.ap-south-1-1.ec2.cloud.redislabs.com',
+        port: 10992
     }
 });
 
